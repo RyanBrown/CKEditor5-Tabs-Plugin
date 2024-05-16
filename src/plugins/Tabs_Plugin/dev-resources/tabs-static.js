fc4f0031bd0ef738774b96e8639fc0b750edfa1f
Ryan Brown <ryanbrown80@gmail.com>
2024-05-16 11:05:57 -0500

    buttons: static file updates

--- src/plugins/Tabs_Plugin/dev-resources/tabs-static.js
+++ src/plugins/Tabs_Plugin/dev-resources/tabs-static.js
@@ -3,18 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const addTabButton = document.querySelector('.add-tab-list-item');
 
     // Function to add a new tab
+    // Function to add a new tab
     function addTab() {
         const newIndex = tabsContainer.querySelectorAll('.tab-list-item').length + 1;
         const newTab = document.createElement('li');
         newTab.className = 'tab-list-item';
         newTab.dataset.target = `#tab${newIndex}`;
         newTab.innerHTML = `
-        <div class="tab-edit-bar">
-          <button class="move-left-button" title="Move Tab Left"><span>Move Tab Left</span></button>
-          <button class="move-right-button" title="Move Tab Right"><span>Move Tab Right</span></button>
-          <div class="delete-tab-button" title="Delete Tab"><span>Delete Tab</span></div>
-        </div>
-        <div class="tab-title">Tab Name ${newIndex}</div>`;
+    <div class="tab-edit-bar">
+      <button class="move-left-button" title="Move Tab Left"><span>Move Tab Left</span></button>
+      <button class="move-right-button" title="Move Tab Right"><span>Move Tab Right</span></button>
+      <div class="delete-tab-button" title="Delete Tab"><span>Delete Tab</span></div>
+    </div>
+    <div class="tab-title">Tab Name ${newIndex}</div>`;
         const newContent = document.createElement('div');
         newContent.id = `tab${newIndex}`;
         newContent.className = 'tab-nested-content';
@@ -50,10 +51,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Remove a tab and its content
     function deleteTab(tab) {
-        const targetContent = document.querySelector(tab.dataset.target);
-        tab.remove();
-        targetContent.remove();
-        updateTabVisibility();
+        const tabs = tabsContainer.querySelectorAll('.tab-list-item');
+        if (tabs.length > 1) {
+            const targetContent = document.querySelector(tab.dataset.target);
+            tab.remove();
+            targetContent.remove();
+            if (!tabsContainer.querySelector('.tab-list-item.active')) {
+                tabsContainer.querySelector('.tab-list-item').classList.add('active');
+                document
+                    .querySelector(tabsContainer.querySelector('.tab-list-item').dataset.target)
+                    .classList.add('active');
+            }
+            updateTabVisibility();
+        }
     }
 
     // Event delegation for handling all tab interactions
