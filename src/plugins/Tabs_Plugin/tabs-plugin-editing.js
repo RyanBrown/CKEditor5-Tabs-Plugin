5bf9666a26147d3a5782c97eb094fbe7d3f1376d
Ryan Brown <ryanbrown80@gmail.com>
2024-06-10 16:53:46 -0500

    tabs-plugin-preview-tables-V2: tabs list item stying updates to match UCE

--- src/plugins/Tabs_Plugin/tabs-plugin-editing.js
+++ src/plugins/Tabs_Plugin/tabs-plugin-editing.js
@@ -49,7 +49,6 @@ export default class TabsPluginEditing extends Plugin {
             allowAttributes: ['class'],
             allowContentOf: '$block',
             allowIn: 'tabListTable_td',
-            inheritAllFrom: '$blockObject', // Prevent nested tab plugin
             isLimit: true,
         });
         schema.register('tabContent', {
