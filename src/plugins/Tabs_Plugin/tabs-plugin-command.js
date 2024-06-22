bd3dce1eac1e61a49f3b6f28aef115bb09e5547d
Ryan Brown <ryanbrown80@gmail.com>
2024-06-22 14:37:26 -0500

    AWT2-3105v2: tabs plugin 2 editing updates

--- src/plugins/Tabs_Plugin/tabs-plugin-command.js
+++ src/plugins/Tabs_Plugin/tabs-plugin-command.js
@@ -1,4 +1,5 @@
 import { Command } from '@ckeditor/ckeditor5-core';
+import { generateId } from './tabs-plugin-utils';
 
 export default class TabsPluginCommand extends Command {
     execute() {
@@ -19,16 +20,66 @@ export default class TabsPluginCommand extends Command {
 }
 
 function createTabsPlugin(writer) {
-    const tabsPlugin = writer.createElement('tabsPlugin');
+    const uniqueId = generateId('plugin-id');
+    const tabsPlugin = writer.createElement('tabsPlugin', { id: uniqueId });
     const containerDiv = writer.createElement('containerDiv');
 
     writer.append(containerDiv, tabsPlugin);
 
-    // You may want to add some default content here
-    // For example:
-    const paragraph = writer.createElement('paragraph');
-    writer.append(paragraph, containerDiv);
-    writer.appendText('Tab content goes here', paragraph);
+    // Create tabHeader
+    const tabHeader = writer.createElement('tabHeader');
+    writer.append(tabHeader, containerDiv);
+
+    // Create tabList
+    const tabList = writer.createElement('tabList');
+    writer.append(tabList, tabHeader);
+
+    // Create initial tabListItem
+    const tabListItem = createTabListItem(writer, uniqueId);
+    writer.append(tabListItem, tabList);
+
+    // Create tabContent
+    const tabContent = writer.createElement('tabContent');
+    writer.append(tabContent, containerDiv);
+
+    // Create initial tabNestedContent
+    const tabNestedContent = createTabNestedContent(writer, uniqueId);
+    writer.append(tabNestedContent, tabContent);
 
     return tabsPlugin;
+}
+
+function createTabListItem(writer, tabContainerId) {
+    const tabListItem = writer.createElement('tabListItem');
+    const dataTarget = `#${tabContainerId}-tab-0`;
+    writer.setAttribute('data-target', dataTarget, tabListItem);
+    writer.setAttribute('data-plugin-id', tabContainerId, tabListItem);
+    writer.setAttribute('class', 'yui3-tab tablinks active', tabListItem);
+
+    const tabListItemLabelDiv = writer.createElement('tabListItemLabelDiv');
+    writer.append(tabListItemLabelDiv, tabListItem);
+
+    const tabListTable = writer.createElement('tabListTable');
+    writer.append(tabListTable, tabListItemLabelDiv);
+
+    // Add table structure (thead, tbody, tr, td) here if needed
+
+    const tabTitle = writer.createElement('tabTitle');
+    writer.appendText('Tab 1', tabTitle);
+    writer.append(tabTitle, tabListTable);
+
+    return tabListItem;
+}
+
+function createTabNestedContent(writer, tabContainerId) {
+    const tabNestedContent = writer.createElement('tabNestedContent');
+    writer.setAttribute('id', `${tabContainerId}-tab-0`, tabNestedContent);
+    writer.setAttribute('data-plugin-id', tabContainerId, tabNestedContent);
+    writer.setAttribute('class', 'yui3-tab-panel tabcontent active', tabNestedContent);
+
+    const paragraph = writer.createElement('paragraph');
+    writer.appendText('Tab content goes here', paragraph);
+    writer.append(paragraph, tabNestedContent);
+
+    return tabNestedContent;
 }
