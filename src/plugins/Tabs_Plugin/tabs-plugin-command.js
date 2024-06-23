08907098139c327099ab42b4ca6d5666516c5fae
Ryan Brown <ryanbrown80@gmail.com>
2024-06-23 13:14:41 -0500

    AWT2-3105v2: multipe plugins active fix

--- src/plugins/Tabs_Plugin/tabs-plugin-command.js
+++ src/plugins/Tabs_Plugin/tabs-plugin-command.js
@@ -25,50 +25,30 @@ export default class TabsPluginCommand extends Command {
         const root = model.document.getRoot();
 
         model.change(() => {
-            let firstInactiveTab = null;
-            let firstInactiveContent = null;
-
             for (const element of root.getChildren()) {
                 if (element.is('element', 'tabsPlugin')) {
                     const containerDiv = element.getChild(0);
                     const tabHeader = containerDiv.getChild(0);
                     const tabList = tabHeader.getChild(0);
-
-                    for (const tabListItem of tabList.getChildren()) {
-                        if (!(tabListItem.getAttribute('class') || '').includes('active')) {
-                            if (!firstInactiveTab) {
-                                firstInactiveTab = tabListItem;
-                            }
-                        } else {
-                            firstInactiveTab = null;
-                            break;
-                        }
-                    }
-
                     const tabContent = containerDiv.getChild(1);
-                    for (const tabNestedContent of tabContent.getChildren()) {
-                        if (!(tabNestedContent.getAttribute('class') || '').includes('active')) {
-                            if (!firstInactiveContent) {
-                                firstInactiveContent = tabNestedContent;
-                            }
-                        } else {
-                            firstInactiveContent = null;
-                            break;
-                        }
+
+                    const firstTabListItem = tabList.getChild(0);
+                    const firstTabNestedContent = tabContent.getChild(0);
+
+                    if (firstTabListItem && !firstTabListItem.hasAttribute('class', 'active')) {
+                        writer.setAttribute(
+                            'class',
+                            (firstTabListItem.getAttribute('class') || '') + ' active',
+                            firstTabListItem
+                        );
                     }
 
-                    if (firstInactiveTab && firstInactiveContent) {
+                    if (firstTabNestedContent && !firstTabNestedContent.hasAttribute('class', 'active')) {
                         writer.setAttribute(
                             'class',
-                            (firstInactiveTab.getAttribute('class') || '') + ' active',
-                            firstInactiveTab
+                            (firstTabNestedContent.getAttribute('class') || '') + ' active',
+                            firstTabNestedContent
                         );
-                        writer.setAttribute(
-                            'class',
-                            (firstInactiveContent.getAttribute('class') || '') + ' active',
-                            firstInactiveContent
-                        );
-                        break;
                     }
                 }
             }
