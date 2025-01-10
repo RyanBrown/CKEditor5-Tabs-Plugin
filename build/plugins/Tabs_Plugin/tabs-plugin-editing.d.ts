467bfcea898180644e31c21a5c2732e831790b7d
Ryan Brown <ryanbrown80@gmail.com>
2025-01-10 15:39:24 -0600

    links-primeng-connect: deleting build

--- build/plugins/Tabs_Plugin/tabs-plugin-editing.d.ts
+++
@@ -1,8 +0,0 @@
-export default class TabsPluginEditing extends Plugin {
-    static get requires(): (typeof Widget)[];
-    init(): void;
-    _defineSchema(): void;
-    _defineConverters(): void;
-}
-import { Plugin } from '@ckeditor/ckeditor5-core';
-import { Widget } from '@ckeditor/ckeditor5-widget';
--- build/plugins/alight-link_plugin/alight-link-plugin-ui.d.ts
+++
@@ -1,5 +0,0 @@
-import { Plugin } from '@ckeditor/ckeditor5-core';
-export default class AlightLinkPluginUI extends Plugin {
-    static get pluginName(): string;
-    init(): void;
-}
