bdd2e9d3ab8c1d62f9ac720f6dc1c624cdff4cff
Ryan Brown <ryanbrown80@gmail.com>
2025-01-23 08:15:10 -0600

    clean-plugins: labels added

--- src/plugins/alight-population-plugin/alight-population-plugin.ts
+++ src/plugins/alight-population-plugin/alight-population-plugin.ts
@@ -5,27 +5,27 @@ import ToolBarIcon from './assets/icon-population.svg';
 import './styles/alight-population-plugin.scss';
 
 export default class AlightPopulationPlugin extends Plugin {
-    init() {
-        const editor = this.editor;
-        const t = editor.t;
+  init() {
+    const editor = this.editor;
+    const t = editor.t;
 
-        // Add a new toolbar button named 'alertButton'.
-        editor.ui.componentFactory.add('alightPopulationPlugin', (locale: Locale) => {
-            const buttonView = new ButtonView(locale);
+    // Add a new toolbar button named 'alertButton'.
+    editor.ui.componentFactory.add('alightPopulationPlugin', (locale: Locale) => {
+      const buttonView = new ButtonView(locale);
 
-            buttonView.set({
-                icon: ToolBarIcon,
-                label: t('Insert Alight Population'),
-                tooltip: true,
-                withText: false,
-            });
+      buttonView.set({
+        icon: ToolBarIcon,
+        label: t('Insert Alight Population'),
+        tooltip: true,
+        withText: false,
+      });
 
-            // Add the click event listener.
-            buttonView.on('execute', () => {
-                alert(t('Hello! This is the "Population" plugin.'));
-            });
+      // Add the click event listener.
+      buttonView.on('execute', () => {
+        alert(t('Hello! This is the "Population" plugin.'));
+      });
 
-            return buttonView;
-        });
-    }
+      return buttonView;
+    });
+  }
 }
