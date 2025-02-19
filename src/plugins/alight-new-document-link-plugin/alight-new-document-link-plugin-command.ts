f10bb230da86fce2f8bcf178158b2b8d9ca4212b
Ryan Brown <ryanbrown80@gmail.com>
2025-02-19 17:11:08 -0600

    new-doc: removing dead code

--- src/plugins/alight-new-document-link-plugin/alight-new-document-link-plugin-command.ts
+++
@@ -1,1 +0,0 @@
-// src/plugins/alight-new-document-link-plugin/alight-new-document-link-plugin-command.ts
--- src/plugins/alight-new-document-link-plugin/alight-new-document-link-plugin-utils.ts
+++
@@ -1,5 +0,0 @@
-// src/plugins/alight-new-document-link-plugin/alight-new-document-link-plugin-utils.ts
-// Currently unused, but can be used for helper functions or utilities in the future.
-export function exampleUtilityFunction(): void {
-  console.log('Utility function called');
-}
