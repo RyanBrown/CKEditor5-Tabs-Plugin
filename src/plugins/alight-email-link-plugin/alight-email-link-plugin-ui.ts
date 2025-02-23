6c606b8f41234fb478dc1964872a8b0f7d19e991
Ryan Brown <ryanbrown80@gmail.com>
2025-02-23 14:31:10 -0600

    email-link-v3: files merged into core plugins

--- src/plugins/alight-email-link-plugin/alight-email-link-plugin-ui.ts
+++ src/plugins/alight-email-link-plugin/alight-email-link-plugin-ui.ts
@@ -1,6 +1,6 @@
 // src/plugins/alight-email-link-plugin/alight-email-link-plugin-ui.ts
 import { Plugin } from '@ckeditor/ckeditor5-core';
-import { ButtonView, ContextualBalloon, View, LabelView, InputTextView, submitHandler } from '@ckeditor/ckeditor5-ui';
+import { ButtonView, ContextualBalloon, View, submitHandler } from '@ckeditor/ckeditor5-ui';
 import { ClickObserver } from '@ckeditor/ckeditor5-engine';
 import { FocusTracker, KeystrokeHandler } from '@ckeditor/ckeditor5-utils';
 import type { Locale } from '@ckeditor/ckeditor5-utils';
@@ -8,94 +8,9 @@ import { CkAlightModalDialog } from '../ui-components/alight-modal-dialog-compon
 import LinkUI from '@ckeditor/ckeditor5-link/src/linkui';
 import ToolBarIcon from '@ckeditor/ckeditor5-link/theme/icons/link.svg';
 import './styles/alight-email-link-plugin.scss';
+import { FormRowView, EmailLinkFormView } from './alight-email-link-plugin-utils';
 
-class FormRowView extends View {
-  public readonly labelView: LabelView;
-  public readonly inputView: InputTextView;
 
-  constructor(locale: Locale, options: { labelText: string; inputAttributes?: Record<string, any> }) {
-    super(locale);
-
-    this.labelView = new LabelView(locale);
-    this.labelView.text = options.labelText;
-
-    this.inputView = new InputTextView(locale);
-    if (options.inputAttributes) {
-      this.inputView.extendTemplate({
-        attributes: {
-          ...options.inputAttributes
-        }
-      });
-    }
-
-    this.setTemplate({
-      tag: 'div',
-      attributes: {
-        class: ['ck', 'ck-form-row']
-      },
-      children: [this.labelView, this.inputView]
-    });
-  }
-
-  focus(): void {
-    this.inputView.focus();
-  }
-}
-
-class EmailLinkFormView extends View {
-  public readonly emailInputRow: FormRowView;
-  public readonly orgNameInputRow: FormRowView;
-  public readonly focusTracker: FocusTracker;
-  public readonly keystrokes: KeystrokeHandler;
-
-  constructor(locale: Locale) {
-    super(locale);
-
-    this.emailInputRow = new FormRowView(locale, {
-      labelText: 'Email address',
-      inputAttributes: { type: 'email', required: true }
-    });
-
-    this.orgNameInputRow = new FormRowView(locale, {
-      labelText: 'Organization name',
-      inputAttributes: { type: 'text' }
-    });
-
-    this.focusTracker = new FocusTracker();
-    this.keystrokes = new KeystrokeHandler();
-
-    this.setTemplate({
-      tag: 'form',
-      attributes: {
-        class: ['ck', 'ck-email-link-form'],
-        tabindex: '-1'
-      },
-      children: [this.emailInputRow, this.orgNameInputRow]
-    });
-  }
-
-  focus(): void {
-    this.emailInputRow.focus();
-  }
-
-  override render(): void {
-    super.render();
-
-    submitHandler({ view: this });
-
-    const elements = [
-      this.emailInputRow.inputView.element,
-      this.orgNameInputRow.inputView.element
-    ];
-    elements.forEach(element => {
-      if (element) {
-        this.focusTracker.add(element);
-      }
-    });
-
-    this.keystrokes.listenTo(this.element!);
-  }
-}
 
 function isValidEmail(email: string): boolean {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
--- src/plugins/alight-email-link-plugin/alight-email-link-plugin-utils.ts
+++ src/plugins/alight-email-link-plugin/alight-email-link-plugin-utils.ts
@@ -1,6 +1,8 @@
 // src/plugins/alight-email-link-plugin/alight-email-link-plugin-utils.ts
 import type { Editor } from '@ckeditor/ckeditor5-core';
 import { type Element } from '@ckeditor/ckeditor5-engine';
+import { View, LabelView, InputTextView, submitHandler } from '@ckeditor/ckeditor5-ui';
+import { Locale, FocusTracker, KeystrokeHandler } from '@ckeditor/ckeditor5-utils';
 
 export function getSelectedLinkElement(editor: Editor): Element | null {
   const view = editor.editing.view;
@@ -25,4 +27,92 @@ export function getSelectedLinkElement(editor: Editor): Element | null {
   }
 
   return null;
+}
+
+export class FormRowView extends View {
+  public readonly labelView: LabelView;
+  public readonly inputView: InputTextView;
+
+  constructor(locale: Locale, options: { labelText: string; inputAttributes?: Record<string, any> }) {
+    super(locale);
+
+    this.labelView = new LabelView(locale);
+    this.labelView.text = options.labelText;
+
+    this.inputView = new InputTextView(locale);
+    if (options.inputAttributes) {
+      this.inputView.extendTemplate({
+        attributes: {
+          ...options.inputAttributes
+        }
+      });
+    }
+
+    this.setTemplate({
+      tag: 'div',
+      attributes: {
+        class: ['ck', 'ck-form-row']
+      },
+      children: [this.labelView, this.inputView]
+    });
+  }
+
+  focus(): void {
+    this.inputView.focus();
+  }
+}
+
+export class EmailLinkFormView extends View {
+  public readonly emailInputRow: FormRowView;
+  public readonly orgNameInputRow: FormRowView;
+  public readonly focusTracker: import('@ckeditor/ckeditor5-utils').FocusTracker;
+  public readonly keystrokes: import('@ckeditor/ckeditor5-utils').KeystrokeHandler;
+
+  constructor(locale: import('@ckeditor/ckeditor5-utils').Locale) {
+    super(locale);
+
+    this.emailInputRow = new FormRowView(locale, {
+      labelText: 'Email address',
+      inputAttributes: { type: 'email', required: true }
+    });
+
+    this.orgNameInputRow = new FormRowView(locale, {
+      labelText: 'Organization name',
+      inputAttributes: { type: 'text' }
+    });
+
+    this.focusTracker = new FocusTracker();
+    this.keystrokes = new KeystrokeHandler();
+
+    this.setTemplate({
+      tag: 'form',
+      attributes: {
+        class: ['ck', 'ck-email-link-form'],
+        tabindex: '-1'
+      },
+      children: [this.emailInputRow, this.orgNameInputRow]
+    });
+  }
+
+  focus(): void {
+    this.emailInputRow.focus();
+  }
+
+  override render(): void {
+    super.render();
+
+    submitHandler({ view: this });
+
+    const elements = [
+      this.emailInputRow.inputView.element,
+      this.orgNameInputRow.inputView.element
+    ];
+    elements.forEach(element => {
+      if (element) {
+        this.focusTracker.add(element);
+      }
+    });
+
+    this.keystrokes.listenTo(this.element!);
+  }
 }
