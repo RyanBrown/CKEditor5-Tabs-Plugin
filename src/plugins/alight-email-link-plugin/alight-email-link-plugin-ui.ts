aea93a23e2d5a598d8bc311a1b6dc5cf91ea413f
Ryan Brown <ryanbrown80@gmail.com>
2025-02-24 11:57:31 -0600

    email-link-v3: removed dead methods

--- src/plugins/alight-email-link-plugin/alight-email-link-plugin-ui.ts
+++ src/plugins/alight-email-link-plugin/alight-email-link-plugin-ui.ts
@@ -289,7 +289,7 @@ export default class AlightEmailLinkPluginUI extends Plugin {
 
     // Create an empty container for the modal content
     const container = document.createElement('div');
-    container.className = 'email-link-content';
+    container.className = 'email-link-modal-content';
 
     // Set the empty container as content
     this._modalDialog.setContent(container);
