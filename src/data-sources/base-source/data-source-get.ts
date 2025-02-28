c9fb73429778faefd926296bff6f24a91f918f69
Ryan Brown <ryanbrown80@gmail.com>
2025-02-28 09:58:26 -0600

    predefined-link-refactor: predefined balloon updates

---
+++ src/data-sources/base-source/data-source-get.ts
@@ -0,0 +1,15 @@
+// import { DataSourceRequest, IDataSourceRequest } from "./data-source-request";
+// import { HttpRequestMethod } from "./http-request-method";
+
+// export class DataSourceGet extends DataSourceRequest implements IDataSourceRequest {
+//   override get requestMethod(): HttpRequestMethod { return HttpRequestMethod.GET; }
+
+//   private _path: string;
+//   override get path(): string { return this._path }
+//   override set path(value: string) { this._path = value; }
+
+//   constructor(host: string, path: string, queryParams?: string) {
+//     super(host, queryParams);
+//     this.path = path;
+//   }
+// }
