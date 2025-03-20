73849dd7d8219900012617b2d086626a07ebbf87
Ryan Brown <ryanbrown80@gmail.com>
2025-03-20 16:08:08 -0500

    data sync updates

--- src/data-loaders/predefined-links-loader.ts
+++ src/data-loaders/predefined-links-loader.ts
@@ -1,1 +1,8 @@
 // src/data-loaders/predefined-links-loader.ts
+// src/data-loaders/categories-loader.ts
+import AlightRequest from '../services/alight-request';
+import { LinksService } from './../services/links-service';
+
+export default self.onmessage = async (event: MessageEvent<AlightRequest>) =>
+  self.postMessage(await new LinksService(event.data).getPredefinedLinks());
+
