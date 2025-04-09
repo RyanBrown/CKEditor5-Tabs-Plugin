d30decc2743149c987e8953bb79c6e9667a78d0e
Ryan Brown <ryanbrown80@gmail.com>
2025-04-09 15:04:50 -0500

    comment updated

--- src/plugins/alight-email-link-plugin/utils.ts
+++ src/plugins/alight-email-link-plugin/utils.ts
@@ -24,7 +24,7 @@ import { upperFirst } from 'lodash-es';
 
 const ATTRIBUTE_WHITESPACES = /[\u0000-\u0020\u00A0\u1680\u180E\u2000-\u2029\u205f\u3000]/g; // eslint-disable-line no-control-regex
 
-// Modified SAFE_URL_TEMPLATE to only allow http and https protocols
+// SAFE_URL_TEMPLATE to only allow http and https protocols
 const SAFE_URL_TEMPLATE = '^(?:(?:<protocols>):|[^a-z]|[a-z+.-]+(?:[^a-z+.:-]|$))';
 
 // Enhanced email detection regex
